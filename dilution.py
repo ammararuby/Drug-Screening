fb4f76e74ef464c15aff58a298841f395434caaa
weissab <andrea.weiss7@gmail.com>
2020-05-07 13:31:24 -0400

    volume table complete

--- dilution.py
+++ dilution.py
@@ -13,13 +13,22 @@ def usage():
 
     - 3 is the number of replicates
     - 200 is the assay volume
-    - mixing_matrix.csv is the file containing the experimental drug combinations coefficient (coded concentrations)
-    - dose_matrix.csv is the file containing the dosing information for each drug (real concentations)
+    - mixing_matrix.csv is the file containing the experimental drug combinations where each row represents a specific drug and each 
+        column represent a different drug combination. Drug concentrations are represented as coefficient or coded concentrations. 
+    - dose_matrix.csv is the file containing the dosing information for each drug (real concentations corresponding to coded concentrations)
     - stock.csv is the file containing the initial stock concentration for each drug
 
+
+        NOTE: if some drugs have fewer than the maximum number of concentrations, always use the highest coded concentrations possible
+        and leave empty lower concentrations. In example, drug 1 has only 1 dose and drug 2 has 3 doses
+        i.e.
+               Dose 1   Dose2   Dose3
+        Drug 1  0       0       5
+        Drug 2  1       2       5
     """)
 
 
+# Parse arguments
 def parse_args(args):
     if len(args) != 6:
         usage()
@@ -33,6 +42,7 @@ def parse_args(args):
     return num_replicates, assay_volume, mixing_matrix, dose_matrix, stock
 
 
+# Define basic variables used in downstream code
 def basic_metrics(mixing_matrix, num_replicates, assay_volume):
     num_drugs, num_mixtures = mixing_matrix.shape[0], mixing_matrix.shape[1]
     # If it starts at 1 or 0
@@ -43,6 +53,7 @@ def basic_metrics(mixing_matrix, num_replicates, assay_volume):
     return num_drugs, num_mixtures, min_num_doses, max_num_doses, base_volume
 
 
+# Construct matrix that counts the number of occurences for each drug at each dose present in all mixtures
 def count_doses(mixing_matrix, min_num_doses, max_num_doses):
     # Initialize
     num_doses = max_num_doses + 1 - min_num_doses
@@ -61,13 +72,25 @@ def count_doses(mixing_matrix, min_num_doses, max_num_doses):
     return dose_count_matrix
 
 
-### Constructing Volume Matrix ###
-def volume_matrix(dose_count_matrix, ):
+# Construct volume matrix. Calculate the volume of each drug dose needed.
+def gen_volume_matrix(dose_count_matrix, base_volume):
+    volume_matrix = dose_count_matrix*int(base_volume)
+    return volume_matrix
     
-    
-    #volumes = count_matrix.drop(['Drugs'], axis = 1) * base_volume
-    #volume_matrix = pd.concat([data[['Drugs']], volumes], axis = 1)
 
+# Construct table of dilution factors. Table containing the dilution factor needed for serial dilutions, i.e. the ratio
+# between each drug dose and the next higest dose. Rhe highest drug dose should be diluted from the stock.
+def dilution_factors(dose_matrix, stock, mixing_matrix, min_num_doses, max_num_doses):
+    # Initialize
+    num_doses = max_num_doses + 1 - min_num_doses
+    num_drugs = len(mixing_matrix.index)
+    dilution_matrix = pd.DataFrame(
+        index=mixing_matrix.index, #rows, i.e. drugs
+        columns=range(min_num_doses, max_num_doses + 1), #columns, i.e. number of doses
+        data=np.zeros((num_drugs, num_doses), dtype=int))
+
+
+    return dilution_matrix
 
 
 
@@ -75,7 +98,10 @@ def main():
     num_replicates, assay_volume, mixing_matrix, dose_matrix, stock = parse_args(sys.argv)
     num_drugs, num_mixtures, min_num_doses, max_num_doses, base_volume = basic_metrics(mixing_matrix, num_replicates, assay_volume)
     dose_count_matrix = count_doses(mixing_matrix, min_num_doses, max_num_doses)
-    print(dose_count_matrix)
+    volume_matrix = gen_volume_matrix(dose_count_matrix, base_volume)
+    dilution_matrix = dilution_factors(dose_matrix, stock, mixing_matrix, min_num_doses, max_num_doses)
+    print(dilution_matrix)
+
 
 
 if __name__ == '__main__':
